ba557a5398f95b095ee0e37c45b750aae389656e
Austin Felipe <austin.santos@evoxcorp.com>
2020-02-12 00:30:17 -0300

    Updates

---
+++ precache-manifest.595834fd7a4183a94005f6818ac2f6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219ab8746dadaf56f8260bc5ee9fea5d",
+    "url": "/react-first-app/index.html"
+  },
+  {
+    "revision": "042548902dc1635b77be",
+    "url": "/react-first-app/static/css/main.a6523fd9.chunk.css"
+  },
+  {
+    "revision": "064b217c46aed3ae562a",
+    "url": "/react-first-app/static/js/2.7a888f72.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-first-app/static/js/2.7a888f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042548902dc1635b77be",
+    "url": "/react-first-app/static/js/main.63b99165.chunk.js"
+  },
+  {
+    "revision": "c9b7472fc1a142f877f9",
+    "url": "/react-first-app/static/js/runtime-main.fa68b7f7.js"
+  }
+]);
